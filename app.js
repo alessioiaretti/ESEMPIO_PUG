bc777adcb5dfde5e68cad555bff88404a82fdf95
alessioiaretti <iaretti.alessio@iisgalvanimi.edu.it>
2019-11-17 15:33:57 +0000

    1

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 const people = require('./people.json'); //Copia il file people.json dentro la variabile people
 var app = express();
 
-app.set('view engine', 'pug');   //Dico a express di usare pug come motore di template
+app.set('view engine', 'pug');   //Dico a express di usare pug come motore di template e quindi andare 
+                                 // nella cartella "views" e usarla come cartella principale 
+                                 // e per trovare le pagine con estensione "pug"
 
-app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
+app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici come 
+                                                // fogli di stile e immagini
 
 app.get('/', function (req, res) {
   res.render('index', {
